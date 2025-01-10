ae8f016564a5fa9963669691a5d30cd0e5f0365b
Daniel Arias <danielsam707@gmail.com>
2025-01-10 15:12:09 -0500

    Clase 46: Calculando el total con ngOnChanges | Tener presente como esta compuesto el objeto SimpleChanges que continen las como claves los nombres de los @inputs que han cambiado

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, Input, } from '@angular/core';
+import { Component, signal, Input, SimpleChanges, } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common'; 
 
@@ -13,9 +13,23 @@ export class HeaderComponent {
 
   hideSideMenu = signal(true);
   @Input({required:true}) cart: Product[] = [];
-  
+  total = signal(0);
+
+  //Este metodo hace que el menu lateral se esconda o aparezca
   toogleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState);
-  }
+    }
 
+    //Este metodo se va a ejecutar cada vez que haya un cambio en el input en este caso el cart
+    ngOnChanges(changes: SimpleChanges) {
+      const cart = changes['cart'];
+      if (cart) {
+        this.total.set(this.calcTotal());
+      }
+    }
+
+    //Metodo para sumar el total de los precios
+    calcTotal() {
+      return this.cart.reduce((total, product)=> total + product.price, 0);
+    }
 }
