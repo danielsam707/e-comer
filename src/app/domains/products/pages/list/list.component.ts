beaee45922c32abae86ed659c4984708fa00efd5
Daniel Arias <danielsam707@gmail.com>
2025-01-13 18:48:23 -0500

    Clase 51: Obteniendo datos de una REST API

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { Product } from './../../../shared/models/product.model'
 import { HeaderComponent } from './../../../shared/components/header/header.component';
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -19,57 +20,28 @@ export class ListComponent {
   // cart = signal<Product[]>([]);
 
 
-  private cartService = inject(CartService);
+  
   products = signal<Product[]>([]);
+  private cartService = inject(CartService);
+  // Se borro una lista de productos
 
-  constructor() {
-    const initProducts: Product[] = [
-      {
-        id: Date.now(),
-        title: 'Pro 1',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=2',
-        creationAt: new Date().toISOString()
+
+  //Injectamos el servicio
+  private productService = inject(ProductService);
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
       },
-      {
-        id: Date.now(),
-        title: 'Pro 2',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=23',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 3',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=3',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 1',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=2',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 2',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=23',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 3',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=3',
-        creationAt: new Date().toISOString()
-      },
-    ];
-    //Asigno el array al signal products
-    this.products.set(initProducts);
+      error: () => {
+
+      }
+    })
   }
+
+
+
 
   // Este es el evento que se recibe desde el componente hijo 
   // en este caso un click en el boton de addToCart para agregar productos al carro
