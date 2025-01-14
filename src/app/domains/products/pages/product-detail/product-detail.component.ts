28ca28662fc939d91796d2a4ba30fae537e2e21a
Daniel Arias <danielsam707@gmail.com>
2025-01-14 16:46:41 -0500

    Clase 62: Obteniendo datos del producto | Se trae la informacion de la API

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, Input, signal } from '@angular/core';
+import { ProductService } from '@shared/services/product.service';
+import { Product } from '@shared/models/product.model';
 @Component({
   selector: 'app-product-detail',
   imports: [],
@@ -8,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class ProductDetailComponent {
 
+  @Input() id?: string;
+  private productService = inject(ProductService);
+  product = signal<Product | null>(null);
+  ngOnInit() {
+    if(this.id) {
+      this.productService.getOne(this.id)
+      .subscribe({
+        next: (product) => {
+          this.product.set(product);
+        }
+      })
+    }
+  }
 }
