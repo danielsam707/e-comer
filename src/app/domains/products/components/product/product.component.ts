86454d5ae1b65b83feb836a43327dd08ce274b26
Daniel Arias <danielsam707@gmail.com>
2025-01-14 09:16:02 -0500

    Clase 53: Construyendo tu propio pipe

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../../shared/models/product.model';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule],
+  imports: [CommonModule, ReversePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
