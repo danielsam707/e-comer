94928cab7f1a83706006327f122bcf3cac8fbf48
Daniel Arias <danielsam707@gmail.com>
2025-01-14 16:03:16 -0500

    Clase 59: Vistas anidadas | Permite crear una plantila base donde tengan los componentes que se repiten como el header, footer, baras laterales, etc

--- src/app/domains/info/pages/not-found/not-found.component.ts
+++ src/app/domains/info/pages/not-found/not-found.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
 
-  
+
 }
--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component, signal, Input, SimpleChanges, inject, } from '@angular/core'
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common'; 
 import { CartService } from '../../services/cart.service';
+import { RouterLinkWithHref } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkWithHref],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
