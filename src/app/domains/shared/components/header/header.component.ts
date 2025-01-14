e5504c0aaacf251f410d3ce2d29d33607c639d4b
Daniel Arias <danielsam707@gmail.com>
2025-01-14 16:11:22 -0500

    Clase 60: Uso del routerLinkActive

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -2,12 +2,12 @@ import { Component, signal, Input, SimpleChanges, inject, } from '@angular/core'
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common'; 
 import { CartService } from '../../services/cart.service';
-import { RouterLinkWithHref } from '@angular/router';
+import { RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLinkWithHref],
+  imports: [CommonModule, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
