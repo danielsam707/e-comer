b78f97fd141f9721049481a67b4c49e3395cddd3
Daniel Arias <danielsam707@gmail.com>
2025-01-15 11:18:04 -0500

    Clase 68: Aplicando LazyLoading

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -12,7 +12,7 @@ import { HighlightDirective } from '@shared/directives/highlight.directive';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export default class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
 
