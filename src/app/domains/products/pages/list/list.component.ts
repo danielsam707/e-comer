8f5d804ab46f8dc36868c026b270668ab817ed33
Daniel Arias <danielsam707@gmail.com>
2025-01-15 08:11:53 -0500

    Clase 65: Mostrando categorias desde la API

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -6,10 +6,12 @@ import { Product } from '@shared/models/product.model'
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [CommonModule, ProductComponent, ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -22,6 +24,9 @@ export class ListComponent {
 
   
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
+
+  //Se injecta el servicio del carrito
   private cartService = inject(CartService);
   // Se borro una lista de productos
 
@@ -29,15 +34,13 @@ export class ListComponent {
   //Injectamos el servicio
   private productService = inject(ProductService);
 
-  ngOnInit() {
-    this.productService.getProducts().subscribe({
-      next: (products) => {
-        this.products.set(products);
-      },
-      error: () => {
+  //Se injecta el servicio de categorias
+  private categoryService = inject(CategoryService);
 
-      }
-    })
+  //Aqui se hace el fetch de los productos y las categorias.
+  ngOnInit() {
+    this.getProducts();
+    this.getCategories();
   }
 
 
@@ -47,5 +50,30 @@ export class ListComponent {
   // en este caso un click en el boton de addToCart para agregar productos al carro
   addToCart(product: Product) {
     this.cartService.addToCart(product);
+  }
+
+
+  //Metodo para obtener producto
+  private getProducts(){
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
+      },
+      error: () => { 
+
+      }
+    })
+  }
+
+  //Metodo para obtener producto
+  private getCategories(){
+    this.categoryService.getAll().subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: () => { 
+
+      }
+    })
   }
 }
